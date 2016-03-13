265d155f81ae26b2b73d23dcaa46d30821c87899
clevertoucan <clevertoucan@gmail.com>
2016-03-13 16:44:45 -0400

    Lesson 2

--- Lesson 2 - Variables/src/Variables.java
+++ Lesson 2 - Variables/src/Variables.java
@@ -2,14 +2,10 @@
 public class Variables {
 
 	public static void main(String[] args) {
-		byte otherNum = (byte) 127;
-		short otherOtherNum = 15;
-		int num = 20;
-		long bigNum = 1;
-		
-		int thing = otherNum + otherOtherNum;
-		System.out.println(thing);
-		
-	}
+		int otherNum = 151;
+		int otherOtherNum = 15;
 
+		double thing = ((double)otherNum) / otherOtherNum;
+		System.out.println(thing);	
+	}
 }
