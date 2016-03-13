6471d4445940e447d4b393f06f99a2a4666a0df0
clevertoucan <clevertoucan@gmail.com>
2016-03-13 16:03:47 -0400

    Lesson 2
    lesson 2

---
+++ Lesson 2 - Variables/src/Variables.java
@@ -0,0 +1,15 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		byte otherNum = (byte) 127;
+		short otherOtherNum = 15;
+		int num = 20;
+		long bigNum = 1;
+		
+		int thing = otherNum + otherOtherNum;
+		System.out.println(thing);
+		
+	}
+
+}
