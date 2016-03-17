d108505c7fbe7d28f3154a74b8dd395864ef6307
clevertoucan <clevertoucan@gmail.com>
2016-03-16 21:12:03 -0400

    Scanner examples!

--- Lesson 5 - Scanners/src/ExampleScanner.java
+++ Lesson 5 - Scanners/src/ExampleScanner.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
 
 public class ExampleScanner {
 
 	public static void main(String[] args) {
-		String poop = "Poop";
-		int toilet = 10;
-		int flush = 50;
-		System.out.println(toilet + flush + poop);
-
+//		String poop = "Poop";
+//		int toilet = 10;
+//		int flush = 50;
+//		System.out.println(toilet + flush + poop);
+		
+		Scanner input = new Scanner(System.in);
+		int userInput = input.nextInt();
+		System.out.println("Josh printed out this integer: " + userInput);
+		input.nextLine();
+		String feces = input.nextLine();
+		System.out.println("Josh printed out this String: " + feces);
+		
 	}
-
 }
