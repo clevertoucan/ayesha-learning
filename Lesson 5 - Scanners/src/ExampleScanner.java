71f86da684970bb0dab33054cfa26d468233aec3
KumaStrut <abmarine97@gmail.com>
2016-03-16 20:35:08 -0400

    Scanner things!

---
+++ Lesson 5 - Scanners/src/ExampleScanner.java
@@ -0,0 +1,12 @@
+
+public class ExampleScanner {
+
+	public static void main(String[] args) {
+		String poop = "Poop";
+		int toilet = 10;
+		int flush = 50;
+		System.out.println(toilet + flush + poop);
+
+	}
+
+}
