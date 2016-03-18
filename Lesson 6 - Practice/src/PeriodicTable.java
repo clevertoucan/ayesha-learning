b9c8e0bb4442aa1a24df80c9208f5c59643523fc
clevertoucan <clevertoucan@gmail.com>
2016-03-17 23:00:39 -0400

    Fixed a few minor inconsistencies

--- Lesson 6 - Practice/src/PeriodicTable.java
+++ Lesson 6 - Practice/src/PeriodicTable.java
@@ -1,85 +1,65 @@
 import java.util.Scanner;
 
-public class PeriodicTable { 
+public class PeriodicTable {
 
 	public static void main(String[] args) {
-		
-       int hydrogen;
-       int helium;
-       int lithium;
-       int beryllium;
-       int boron;
-       int carbon;
-       int nitrogen;
-       int oxygen;
-       int flourine;
-       int neon;
-       
-       hydrogen = 1;
-       helium = 2;
-       lithium = 3;
-       beryllium = 4;
-       boron = 5;
-       carbon = 6;
-       nitrogen = 7;
-       oxygen = 8;
-       flourine = 9;
-       neon = 10;
-       
-       
-      Scanner input = new Scanner(System.in);
-       int userInput = input.nextInt();
-       
-      if(userInput == hydrogen){
-       System.out.println("Hydrogen!"); }
-      
-       
-      if(userInput == helium) {
-       System.out.println("Helium!"); }
-       
-      if(userInput == lithium) {
-       System.out.println("Lithium!"); }
-       
-      if(userInput == beryllium) {
-       System.out.println("Beryllium!"); }
-       
-      if(userInput == boron) {
-     System.out.println("Boron!"); }
-     
-      if(userInput == carbon) {
-     System.out.print("Carbon!"); }
-     
-     if(userInput == nitrogen){
-    	 System.out.println("Nitrogen!");
-     }
-     
-     if(userInput == oxygen) {
-    	 System.out.println("Oxygen!");
-     }
-       
-      if(userInput == flourine) {
-    	  System.out.println("Flourine!");
-      }
-      
-      if(userInput == neon) {
-    	  System.out.println("Neon!");
-      }
-      
-       
-    	   
-       
-    	   
-       }
-       
-     
-     
-      
-      
-      
-      
+
+		int hydrogen, helium, lithium, beryllium, boron, carbon, nitrogen, oxygen, flourine, neon;
+
+		hydrogen = 1;
+		helium = 2;
+		lithium = 3;
+		beryllium = 4;
+		boron = 5;
+		carbon = 6;
+		nitrogen = 7;
+		oxygen = 8;
+		flourine = 9;
+		neon = 10;
+
+		Scanner input = new Scanner(System.in);
+		int userInput = input.nextInt();
+
+		if (userInput == hydrogen) {
+			System.out.println("Hydrogen!");
+		}
+
+		if (userInput == helium) {
+			System.out.println("Helium!");
+		}
+
+		if (userInput == lithium) {
+			System.out.println("Lithium!");
+		}
+
+		if (userInput == beryllium) {
+			System.out.println("Beryllium!");
+		}
+
+		if (userInput == boron) {
+			System.out.println("Boron!");
+		}
+
+		if (userInput == carbon) {
+			System.out.print("Carbon!");
+		}
+
+		if (userInput == nitrogen) {
+			System.out.println("Nitrogen!");
+		}
+
+		if (userInput == oxygen) {
+			System.out.println("Oxygen!");
+		}
+
+		if (userInput == flourine) {
+			System.out.println("Flourine!");
+		}
+
+		if (userInput == neon) {
+			System.out.println("Neon!");
+		}
+
 	}
 
-
-	
-
-
+}
