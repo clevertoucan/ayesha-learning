4dd5be7dd0e21e207360502bb410496f967d9184
KumaStrut <abmarine97@gmail.com>
2016-03-19 22:45:23 -0400

    Hooray we did some thangs with stuff with methods!!1111!1!1!1!420!69

--- Lesson 6 - Practice/src/PeriodicTable.java
+++ Lesson 6 - Practice/src/PeriodicTable.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class PeriodicTable {
 
+	
 	public static void main(String[] args) {
-
+        /*
 		int hydrogen, helium, lithium, beryllium, boron, carbon, nitrogen, oxygen, flourine, neon;
 
 		hydrogen = 1;
@@ -59,7 +60,78 @@ public class PeriodicTable {
 		if (userInput == neon) {
 			System.out.println("Neon!");
 		}
-
+*/
+		
+	PeriodicTable.periodicTable();
+	PeriodicTable.periodicTable();	
+	PeriodicTable.periodicTable();
+	PeriodicTable.periodicTable();
+	PeriodicTable.periodicTable();
+	PeriodicTable.periodicTable();
+	PeriodicTable.periodicTable();
+	PeriodicTable.periodicTable();
+	PeriodicTable.periodicTable();
+	PeriodicTable.periodicTable();
+	
 	}
+    public static void periodicTable() {
+    	int hydrogen, helium, lithium, beryllium, boron, carbon, nitrogen, oxygen, flourine, neon;
 
+		hydrogen = 1;
+		helium = 2;
+		lithium = 3;
+		beryllium = 4;
+		boron = 5;
+		carbon = 6;
+		nitrogen = 7;
+		oxygen = 8;
+		flourine = 9;
+		neon = 10;
+
+		Scanner input = new Scanner(System.in);
+		int userInput = input.nextInt();
+
+		if (userInput == hydrogen) {
+			System.out.println("Hydrogen!");
+		}
+
+		if (userInput == helium) {
+			System.out.println("Helium!");
+		}
+
+		if (userInput == lithium) {
+			System.out.println("Lithium!");
+		}
+
+		if (userInput == beryllium) {
+			System.out.println("Beryllium!");
+		}
+
+		if (userInput == boron) {
+			System.out.println("Boron!");
+		}
+
+		if (userInput == carbon) {
+			System.out.print("Carbon!");
+		}
+
+		if (userInput == nitrogen) {
+			System.out.println("Nitrogen!");
+		}
+
+		if (userInput == oxygen) {
+			System.out.println("Oxygen!");
+		}
+
+		if (userInput == flourine) {
+			System.out.println("Flourine!");
+		}
+
+		if (userInput == neon) {
+			System.out.println("Neon!");
+		}
+
+    }
 }
+
+
