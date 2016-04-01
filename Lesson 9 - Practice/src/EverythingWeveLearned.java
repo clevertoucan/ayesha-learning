b04984624da2efe34bcde2222b447de137d804cc
clevertoucan <clevertoucan@gmail.com>
2016-04-01 14:29:36 -0400

    Practice with most of the things we've learned so far

---
+++ Lesson 9 - Practice/src/EverythingWeveLearned.java
@@ -0,0 +1,15 @@
+/*
+ * Make a method called scanPrint with no parameters to scan in 20 different strings (use a loop) from user input 
+ * and concatenate them into one string.
+ * Once you have the string, print it out.
+ * (Hint: to make a method, use the same syntax as the main method below.)
+ */
+public class EverythingWeveLearned {
+	
+	public static void main(String[] args) {
+		
+		scanPrint();
+		
+	}
+
+}
