15aa4d573d16c4f9a1552eeba30c5ee4b1f17388
clevertoucan <clevertoucan@gmail.com>
2016-05-19 23:42:32 +0000

    More on return stuff

--- Lesson 10 - More Operations/src/ReturnExample.java
+++ Lesson 10 - More Operations/src/ReturnExample.java
@@ -3,12 +3,28 @@
  */
 public class ReturnExample {
 
-    public static void main(String[] args){
-        System.out.println( (3 * add(5,6)) );
+    public static void main(String[] args) {
+        System.out.println((3 * add(5, 6)));
+        System.out.println(concatenate("dog","cat"));
+        System.out.println(evaluateTruth(true, false));
     }
 
-    public static int add(int a, int b){
+    public static int add(int a, int b) {
         int aPlusB = a + b;
         return aPlusB;
+    }
+
+    public static String concatenate(String string1, String string2){
+        String aPlusB = string1 + string2;
+        if(evaluateTruth(true, false)) {
+            return aPlusB;
+        } else {
+            return "I like big butts and I cannot lie";
+        }
+    }
+
+    public static boolean evaluateTruth(boolean bool1, boolean bool2){
+        boolean aAndB = bool1 && bool2;
+        return aAndB;
     }
 }
